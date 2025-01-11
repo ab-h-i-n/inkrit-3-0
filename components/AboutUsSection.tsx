9d6a713c1d45d11f3bb7bb70d37ffbbb722c44e8
ab-h-i-n <abhinshelly03@gmail.com>
2025-01-11 13:38:06 +0530

    removed boxrevel from about us section

--- components/AboutUsSection.tsx
+++ components/AboutUsSection.tsx
@@ -7,25 +7,23 @@ export default function AboutUsSection() {
   return (
     <main className="min-h-[100vh] overflow-y-scroll pt-20 pb-12 relative bg-black">
       <div className="container mx-auto px-6 space-y-8">
-        <div className="font-ahsing text-[2.5rem] font-bold text-white text-center">
-          About Us
-        </div>
+        <BoxReveal boxColor="white">
+          <div className="font-ahsing text-[2.5rem] font-bold text-white text-center">
+            About Us
+          </div>
+        </BoxReveal>
         <div className="divide-y divide-white/70 space-y-4 font-poppinsRegular">
           <motion.div
             initial={{ opacity: 0, y: 20 }}
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5 }}
           >
-            <BoxReveal boxColor="white">
-              <h2 className="text-3xl text-white font-bold mb-4 font-poppinsBold">
-                {aboutData.fest.title}
-              </h2>
-            </BoxReveal>
-            <BoxReveal boxColor="white">
-              <p className="text-white/50 mb-4 font-poppinsBold text-justify">
-                {aboutData.fest.description}
-              </p>
-            </BoxReveal>
+            <h2 className="text-3xl text-white font-bold mb-4 font-poppinsBold">
+              {aboutData.fest.title}
+            </h2>
+            <p className="text-white/50 mb-4 font-poppinsBold text-justify">
+              {aboutData.fest.description}
+            </p>
           </motion.div>
 
           <motion.section
@@ -34,16 +32,12 @@ export default function AboutUsSection() {
             transition={{ duration: 0.5 }}
             className=""
           >
-            <BoxReveal boxColor="white">
-              <h2 className="text-3xl text-white font-bold my-4">
-                {aboutData.college.name}
-              </h2>
-            </BoxReveal>
-            <BoxReveal boxColor="white/50">
-              <p className="text-white/50 mb-4 text-justify">
-                {aboutData.college.description}
-              </p>
-            </BoxReveal>
+            <h2 className="text-3xl text-white font-bold my-4">
+              {aboutData.college.name}
+            </h2>
+            <p className="text-white/50 mb-4 text-justify">
+              {aboutData.college.description}
+            </p>
           </motion.section>
 
           <motion.section
@@ -51,16 +45,12 @@ export default function AboutUsSection() {
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5 }}
           >
-            <BoxReveal boxColor="white">
-              <h2 className="text-3xl text-white font-bold my-4">
-                {aboutData.department.name}
-              </h2>
-            </BoxReveal>
-            <BoxReveal boxColor="white">
-              <p className="text-white/50 mb-4 text-justify">
-                {aboutData.department.description}
-              </p>
-            </BoxReveal>
+            <h2 className="text-3xl text-white font-bold my-4">
+              {aboutData.department.name}
+            </h2>
+            <p className="text-white/50 mb-4 text-justify">
+              {aboutData.department.description}
+            </p>
           </motion.section>
         </div>
       </div>
