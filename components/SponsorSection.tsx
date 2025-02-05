21e5d7f4fdb5944fa70915a7f3f510c266cb4d3f
ab-h-i-n <abhinshelly@gmail.com>
2025-02-05 11:43:10 +0530

    fix grid cols sponsors

--- components/SponsorSection.tsx
+++ components/SponsorSection.tsx
@@ -5,11 +5,11 @@ import React from "react";
 
 function SponsorSection({ sponsors }: { sponsors: Sponsor[] }) {
   return (
-    <div className="h-[50vh] pt-20 pb-12 px-6  bg-black/70 backdrop-blur-md">
+    <div className="min-h-[50vh] pt-20 pb-12 px-6  bg-black/70 backdrop-blur-md">
       <h2 className="text-4xl text-center font-ahsing text-white">Sponsors</h2>
-      <div className="w-full h-full flex flex-col justify-center items-center">
+      <div className="w-full h-full flex flex-col justify-center items-center mt-10">
         {sponsors.length > 0 ? (
-          <div className="grid gap-3 grid-flow-col">
+          <div className="grid gap-5 grid-cols-2 sm:grid-cols-3 md:grid-flow-col">
             {sponsors.map((sponsor, index) => (
               <Link href={sponsor.website ?? ""} key={"sponsor" + index}>
                 <Image
